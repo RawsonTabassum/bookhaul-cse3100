d72ff2b60e2fcf6a9a880678a3f5da91716bfdef
Rawson Tabassum <rawson17113@gmail.com>
2022-04-13 23:59:06 +0600

    removed paginator

--- Bookstore/views.py
+++ Bookstore/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 from store.models import Product
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
+
 def home(request):
-    books = Product.objects.all().filter(is_available = True)
+    books = Product.objects.all().filter(is_available=True)
     paginator = Paginator(books, 12)
     page = request.GET.get('page')
     paged_book = paginator.get_page(page)
+
     context = {
         'books': paged_book,
     }
