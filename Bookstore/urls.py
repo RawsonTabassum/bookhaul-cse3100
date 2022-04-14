64a024152bfdef4356662e158f535be2a7e4b017
Rawson Tabassum <rawson17113@gmail.com>
2022-04-14 15:07:57 +0600

    again added url

--- Bookstore/urls.py
+++ Bookstore/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 from . import views
 
 from django.views.static import serve
-# from django.conf.urls import url
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,8 +17,8 @@ urlpatterns = [
 
     path('orders/', include('orders.urls')),
 
-    # url(r'^media/(?P<path>.*)$', serve,
-    #     {'document_root':       settings.MEDIA_ROOT}),
-    # url(r'^static/(?P<path>.*)$', serve,
-    #     {'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
