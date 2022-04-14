8ef7565913406208f10ef6b4da5973035cb5dee6
Rawson Tabassum <rawson17113@gmail.com>
2022-04-14 19:37:18 +0600

    added phone number cause ahhhh

--- accounts/models.py
+++ accounts/models.py
@@ -24,13 +24,14 @@ class my_account_manager(BaseUserManager):
         user.save(using=self.db)
         return user
 
-    def create_superuser(self, first_name, last_name, email, username, password):
+    def create_superuser(self, first_name, last_name, email, username, phone_no, password):
         user = self.create_user(
             email=self.normalize_email(email),
             username=username,
             password=password,
             first_name=first_name,
             last_name=last_name,
+            phone_no=phone_no,
         )
 
         user.is_admin = True
